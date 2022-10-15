7dd4e64ef19a6acd89cdc59176a823c13d6b5b92
yen vy <2054052084vy@ou.edu.vn>
2022-10-15 15:12:03 +0700

    Them xu ly button tru

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,21 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void btCong_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKetQua.Text = c.ToString();
+        }
+
+        private void btTru_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a - b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
