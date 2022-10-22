57402736a46ff874010fb9a43755bbec557eb9fd
vydny <2054052084vy@ou.edu.vn>
2022-10-22 15:15:01 +0700

    Unit Test

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -19,10 +19,13 @@ namespace Calculator
 
         private void btCong_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(txtA.Text);
-            double b = double.Parse(txtB.Text);
-            double c = a + b;
-            txtKetQua.Text = c.ToString();
+            if (txtA.Text != String.Empty)
+            {
+                double a = double.Parse(txtA.Text);
+                double b = double.Parse(txtB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
